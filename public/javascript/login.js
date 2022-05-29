e0f320dc84c04a8b67aee0ebf8820bd1f3e054f4
Beimy <arbm159357@gmail.com>
2022-05-29 15:55:12 -0400

    users are now signed in and sent to their dashboard when they signup

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,52 +1,74 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
-    if(username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+  if(username && email && password) {
+      const response = await fetch('/api/users', {
+          method: 'post',
+          body: JSON.stringify({
+              username,
+              email,
+              password
+          }),
+          headers: { 'Content-Type': 'application/json' }
+      });
 
-        //check response status
-        if(response.ok) {
-            console.log('success');
-        } else {
-            alert(response.statusText);
-        }
-    }
+      //check response status
+      if(response.ok) {
+          console.log('success');
+          autoLogin(email, password, username);
+      } else {
+          alert(response.statusText);
+      }
+  }
 }
 
 async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
+  }
+}
+
+async function autoLogin(email, password, username) {
+  
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+      alert(`Thank you for signing up ${username}, welcome to your dashboard!`)
+    } else {
+      alert(response.statusText);
+    }
+  }
 }
   
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
