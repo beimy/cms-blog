b1a6953273153024a48b703f2e9cd5c8aa815d8a
Beimy <arbm159357@gmail.com>
2022-05-29 14:45:23 -0400

    fixed issue with not allowing users to sign up

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,9 @@
 const withAuth = (req, res, next) => {
     if(!req.session.user_id) {
+        console.log('no auth');
         res.redirect('/login');
     } else {
+        console.log('has auth');
         next();
     }
 };
